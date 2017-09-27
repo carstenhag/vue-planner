ee0f69e856619e9281a999a1ece95ccadc635679
Carsten Hagemann <carsten@chagemann.de>
2017-09-27 19:14:02 +0200

    Add detection if data is available to the Aktuell component

--- src/components/Aktuell.vue
+++ src/components/Aktuell.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   .aktuell
-    p Noch nicht implementiert.
+    p(v-if="dataUnavailable") Bitte das erste mal die Veranstaltungen und die Vorlesungen aktualisieren.
+    template(v-else)
+      p Geht noch nicht ¯\_(ツ)_/¯
 </template>
 
 <script>
@@ -8,10 +10,38 @@
     name: 'Aktuell',
     data () {
       return {
+        dataUnvailable: true
       }
     },
     metaInfo: {
       title: 'Aktuell'
+    },
+    computed: {
+      getEvents () {
+        return this.$store.state.events
+      },
+      getLecturesGroupedByDate () {
+        return this.$store.state.groupedLectures
+      }
+    },
+    created () {
+      this.updateDataStatus()
+    },
+    methods: {
+      updateDataStatus () {
+        console.log(this.getEvents)
+        console.log(this.getLecturesGroupedByDate)
+        // https://stackoverflow.com/a/4994265/3991578
+        if (this.getEvents.length === 0 || Object.keys(this.getLecturesGroupedByDate).length === 0) {
+          this.dataUnavailable = true
+        } else {
+          this.dataUnavailable = false
+        }
+      },
+      printElement (el) {
+        console.log(el)
+        return el
+      }
     }
   }
 </script>
